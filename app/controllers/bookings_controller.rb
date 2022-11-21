fe804f5a2adbe1c24278279fde3c92ce44020be7
Benjamin Doberset <benjamin.doberset.pro@gmail.com>
2022-11-21 17:19:57 +0100

    create raclette controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,19 @@
 class BookingsController < ApplicationController
+  before_action :set_raclette, only: %i[new create update destroy]
+
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(params_booking)
+    @booking.raclette = @raclette
+    @booking.save
+    redirect_to raclette_path(@raclette)
   end
 
   def index
+
   end
 
   def edit
@@ -16,4 +24,15 @@ class BookingsController < ApplicationController
 
   def show
   end
+private
+
+  def set_booking
+    @raclette = Raclette.find(params[:raclette_id])
+
+  end
+
+  def params_booking
+    parmas.require[:bookings].permit(:date, :description)
+  end
+
 end
