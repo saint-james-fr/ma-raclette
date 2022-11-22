407198e3f0f39179d1a1afbb43cd97d712172460
Gaspard Fauchille <gaspard.fauchille@gmail.com>
2022-11-22 17:07:10 +0100

    added pundit for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,12 +4,14 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(params_booking)
     @booking.user = current_user
     @booking.raclette = @raclette
+    authorize @booking
     if @booking.save
       redirect_to raclette_booking_path(@raclette, @booking)
     else
@@ -18,20 +20,24 @@ class BookingsController < ApplicationController
   end
 
   def index
+    @bookings = policy_scope(Booking)
   end
 
   def edit
+    authorize @booking
   end
 
   def update
+    authorize @booking
     @booking.update(params_booking)
     redirect_to raclette_booking_path(@raclette, @booking)
   end
 
   def show
+    authorize @booking
   end
 
-private
+  private
 
   def set_booking
     @booking = Booking.find(params[:id])
