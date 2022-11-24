a64d2a105daf410fd4f111f57fa67fd69e1a3d6d
Gaspard Fauchille <gaspard.fauchille@gmail.com>
2022-11-24 14:20:37 +0100

    added types of eaters

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -48,7 +48,7 @@ class BookingsController < ApplicationController
   end
 
   def params_booking
-    params.require(:booking).permit(:date, :description, :raclette_id, :status)
+    params.require(:booking).permit(:date, :description, :raclette_id, :status, :big_eater, :normal_eater, :small_eater)
   end
 
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,11 @@ class Booking < ApplicationRecord
 
   validates :date, presence: true
   validates :description, presence: true
+  validates :sum_eater, presence: true
+
+  def sum_eater
+    if (small_eater.to_i + normal_eater.to_i + big_eater.to_i) <= raclette.number_of_guests
+      errors.add(:base, 'sum of the eaters must be lower or equal to the number of possible guests for the event')
+    end
+  end
 end
