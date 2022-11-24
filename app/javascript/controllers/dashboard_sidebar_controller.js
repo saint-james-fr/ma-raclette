d559150af4d287c55de6f8fe3aa068777c567a96
Maxence Robinet <maxencerobinet@gmail.com>
2022-11-24 18:53:52 +0100

    edit profile and other stuff

--- app/javascript/controllers/dashboard_sidebar_controller.js
+++ app/javascript/controllers/dashboard_sidebar_controller.js
@@ -9,33 +9,16 @@ export default class extends Controller {
   activeRaclette() {
     const raclette = document.getElementById("dashboard_raclettes")
     const bookings = document.getElementById("dashboard_bookings")
-    const profile = document.getElementById("dashboard_profile")
     raclette.classList.add("display")
     bookings.classList.remove("display")
-    profile.classList.remove("display")
     bookings.classList.add("display-none")
-    profile.classList.add("display-none")
   }
 
   activeBookings() {
     const raclette = document.getElementById("dashboard_raclettes")
     const bookings = document.getElementById("dashboard_bookings")
-    const profile = document.getElementById("dashboard_profile")
     bookings.classList.add("display")
     raclette.classList.remove("display")
-    profile.classList.remove("display")
-    raclette.classList.add("display-none")
-    profile.classList.add("display-none")
-  }
-
-  activeProfile() {
-    const raclette = document.getElementById("dashboard_raclettes")
-    const bookings = document.getElementById("dashboard_bookings")
-    const profile = document.getElementById("dashboard_profile")
-    profile.classList.add("display")
-    bookings.classList.remove("display")
-    raclette.classList.remove("display")
-    bookings.classList.add("display-none")
     raclette.classList.add("display-none")
   }
 }
